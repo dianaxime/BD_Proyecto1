2c2d85f9a7bb727210f6d014c48c7f81bd6df00d
CamilaGO <gon18398@uvg.edu.gt>
2020-03-21 22:21:15 -0600

    Ya se puede desactivar track

--- Conectar python a postgres/Prueba2.py
+++ Conectar python a postgres/Prueba2.py
@@ -25,18 +25,39 @@ def conectar():
         db_version = cur.fetchone()
         # Se muestra la versión por pantalla
         print(db_version)
-        titulo="Back to Black"
-        aName="Amy Winehouse"
-        cur.execute( "SELECT * FROM album WHERE album.title=%s AND album.artistid IN (SELECT artist.artistid FROM artist WHERE artist.name = %s)",(titulo, aName,))
-        for a,b,c in cur.fetchall() :
-            print(a,b,c)
-
-        print("--------------------------------------------------")
-        cur.execute( "SELECT * FROM artist WHERE artist.name = %s",(aName,))
-        for a,b in cur.fetchall() :
-            print(a,b)
-
-        print("--------------------------------------------------")
+        nombre="Balls to the Wall"
+        cur.execute("SELECT actividad_track.trackid FROM actividad_track WHERE actividad_track.trackid IN (SELECT track.trackid FROM track WHERE track.name = %s)",(nombre,))
+        IDTrack=cur.fetchall()
+        print(IDTrack)
+        if(len(IDTrack)!=0):
+            #Si si existe se obtine el ID y adquiere su estado
+            IDoficial=(IDTrack[0][0])
+            print(IDoficial)
+            cur.execute("SELECT actividad_track.esta_activo FROM actividad_track WHERE actividad_track.trackid = %s",(IDoficial,))
+            estado=cur.fetchall()
+            actualState=(estado[0][0])
+            print(actualState)
+            if(actualState==True):
+                #Si esta activada se desactiva el track
+                newState = False
+                print(newState)
+                cur.execute("UPDATE actividad_track SET esta_activo = False WHERE actividadid = %s",(IDoficial,))
+                cur.execute("SELECT * FROM actividad_track ORDER BY actividad_track.trackid ASC LIMIT 10")
+                for a,b,c in cur.fetchall() :
+                    print(a,b,c)
+                print("--------------------------------------------------")
+            else:
+                #Si esta desactivda se activa el track
+                newState = True
+                print(newState)
+                cur.execute("UPDATE actividad_track SET esta_activo = True WHERE actividadid = %s",(IDoficial,))
+                cur.execute("SELECT * FROM actividad_track ORDER BY actividad_track.trackid ASC LIMIT 10")
+                for a,b,c in cur.fetchall() :
+                    print(a,b,c)
+                print("--------------------------------------------------")
+        else:
+            #Sino existe se muestra error
+            print("No existe")
         """print("Artista")
         cur.execute("SELECT * FROM artist LIMIT 10")
         # Recorremos los resultados y los mostramos
