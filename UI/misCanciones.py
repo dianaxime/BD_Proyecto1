77094144647d0b76ac7e5095839ef598b8b3ee07
Maria Vasquez <maria.ines.vf@gmail.com>
2020-05-20 10:29:41 -0600

    tabla reproduccion de canciones para administracion terminado

--- UI/misCanciones.py
+++ UI/misCanciones.py
@@ -82,8 +82,49 @@ class Ui_MisCanciones(object):
                 print('Conexión finalizada.')
 
     def OpenLink(self,item):
+        conexion = None
         if item.column() == 4:
-            webbrowser.open_new(item.text())
+            try:
+                # Lectura de los parámetros de conexion
+                params = config()
+
+                #print(params)
+                # Conexion al servidor de PostgreSQL
+                print('Conectando a la base de datos PostgreSQL...')
+                conexion = psycopg2.connect(**params)
+
+                # creación del cursor
+                cur = conexion.cursor()
+
+                # Ejecución la consulta para obtener la conexión
+                print('La version de PostgreSQL es la:')
+                cur.execute('SELECT version()')
+
+                # Se obtienen los resultados
+                db_version = cur.fetchone()
+                # Se muestra la versión por pantalla
+                print(db_version)
+               
+                webbrowser.open_new(item.text())
+                r=item.row()
+                nombretrack=self.tableWidget.item(r,0).text()
+                print(nombretrack)
+            
+                cur.execute( """SELECT trackid from track where track.name=%s """,(nombretrack,))
+                idtrack=cur.fetchall()
+                idtrack=idtrack[0][0]
+                print(idtrack)
+                cur.execute( """INSERT into reproduccion (trackid) VALUES (%s) """,(idtrack,))
+                conexion.commit()
+                cur.close()                
+                
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+            finally:
+                if conexion is not None:
+                    conexion.close()
+                    print('Conexión finalizada.')
+
 
     def setupUi(self, Form):
         Form.setObjectName("MisCanciones")
@@ -127,7 +168,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
-    ui = Ui_MisCanciones()
+    ui = Ui_MisCanciones(61)
     ui.setupUi(Form)
     Form.show()
     sys.exit(app.exec_())
