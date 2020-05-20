32180e70bcddbf54f5306e5ea1dc08a2c87a0b27
Maria Vasquez <maria.ines.vf@gmail.com>
2020-05-20 11:04:42 -0600

    conectar a mongo

--- UI/misCanciones.py
+++ UI/misCanciones.py
@@ -168,7 +168,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
-    ui = Ui_MisCanciones(61)
+    ui = Ui_MisCanciones()
     ui.setupUi(Form)
     Form.show()
     sys.exit(app.exec_())
--- UI/recomendaciones.py
+++ UI/recomendaciones.py
@@ -13,6 +13,12 @@ from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtWidgets import QTableWidget,QTableWidgetItem
 import psycopg2
 from config import config
+import datetime
+import pymongo
+#conectar a mongo
+conexion=pymongo.MongoClient() 
+db=conexion.proyectoBD
+coleccion=db.recomendaciones
 
 class Ui_Recomendaciones(object):
     def setupUi(self, Form):
@@ -82,14 +88,11 @@ class Ui_Recomendaciones(object):
             # Lectura de los parámetros de conexion
             params = config()
 
-            #print(params)
             # Conexion al servidor de PostgreSQL
             print('Conectando a la base de datos PostgreSQL...')
             conexion = psycopg2.connect(**params)
-
             # creación del cursor
             cur = conexion.cursor()
-
             # Ejecución la consulta para obtener la conexión
             print('La version de PostgreSQL es la:')
             cur.execute('SELECT version()')
@@ -98,41 +101,52 @@ class Ui_Recomendaciones(object):
             db_version = cur.fetchone()
             # Se muestra la versión por pantalla
             print(db_version)
-    
-            if fecha != '' :
-                #CAMBIAR QUERY
-                cur.execute( "SELECT track.name FROM track WHERE track.name=%s",(nombreTrack,))
-                IDtrackO=cur.fetchall()#[0][0]
-                if (len(IDtrackO)==0):
-                    blank=QMessageBox()
-                    blank.setIcon(QMessageBox.Information)
-                    blank.setWindowTitle("ERROR")
-                    blank.setText("El track no esta registrado")
-                    blank.exec()
-                else:
-                    cur.execute("""
-                            SELECT track.name, album.title, artist.name, track.unitprice
-                            FROM track  
-                                JOIN album ON album.albumid = track.albumid
-                                JOIN artist ON artist.artistid = album.artistid
-                            WHERE track.name = %s
-                            """,(nombreTrack,))
+            #fecha = '2009-01-06'
+            if (fecha!=''):
+                ahora = datetime.datetime.utcnow()
+                tiempo = ahora - datetime.timedelta(days=30)
+                cur.execute('''
+                    SELECT customer.email, track."name", 
+                    genre."name" as genre, invoice.invoicedate
+                    FROM invoice
+                    JOIN invoiceline ON invoiceline.invoiceid = invoice.invoiceid
+                    join track on invoiceline.trackid = track.trackid
+                    join genre on track.genreid = genre.genreid
+                    join customer on customer.customerid = invoice.customerid
+                    where invoice.invoicedate = %s
+                ''',(fecha,))
+                # Recorremos los resultados y los mostramos
+                compras = cur.fetchall()
+                for email, cancion, genero, fechac in compras:
+                    #print(email, cancion, genero, fechac)
+                    cur.execute('''
+                        SELECT DISTINCT(track."name"), genre."name"
+                        FROM bitacora
+                        JOIN track ON bitacora.nombre_object = track."name"
+                        JOIN genre ON track.genreid = genre.genreid
+                        WHERE bitacora.nombre_object <> %s
+                        AND bitacora.email <> %s 
+                        AND bitacora.date_on > %s
+                        AND bitacora.date_on < %s
+                        AND bitacora.accion = %s
+                    ''',(cancion, email, tiempo, ahora, 'add'))
+                    resul = cur.fetchall()
+                    for a, b in resul:
+                        #print(a, b)
+                        if (coleccion.find({'_id': email}).count() > 0):
+                            if (coleccion.find({ '_id': email, 'sells.track': cancion}).count() <= 0):
+                                result = coleccion.update({'_id': email }, { '$set': { 'modified': ahora },'$push': { 'sells': {'email': email, 'track': cancion, 'genre': genero, 'date': fechac}}}, w=1)
+                            if (coleccion.find({ '_id': email, 'tracks.track': a}).count() <= 0):
+                                result = coleccion.update({'_id': email }, { '$set': { 'modified': ahora },'$push': { 'tracks': {'track': a, 'genre': b, 'date': ahora} }}, w=1)
+                        else:
+                            result = coleccion.insert({'_id': email }, { '$set': { 'modified': ahora },'$push': { 'sells': {'email': email, 'track': cancion, 'genre': genero, 'date': fechac}, 'tracks': {'track': a, 'genre': b, 'date': ahora} } }, w=1)
+                
+                print("--------------------------------------------------")
 
-                    row = 0
-                    for a,b,c,d in cur.fetchall():
-                        self.tableWidget.setRowCount(row + 1)
-                        self.tableWidget.setItem(row, 0, QTableWidgetItem(a))
-                        self.tableWidget.setItem(row, 1, QTableWidgetItem(b))
-                        self.tableWidget.setItem(row, 2, QTableWidgetItem(c))
-                        self.tableWidget.setItem(row, 3, QTableWidgetItem(str(d)))
-                        row += 1
-                    cur.close()                
+                # Cerremos el cursor
+                cur.close()
             else:
-                blank=QMessageBox()
-                blank.setIcon(QMessageBox.Information)
-                blank.setWindowTitle("INCOMPLETO")
-                blank.setText("Por favor llene los campos")
-                blank.exec()
+                print ("NECESITA FECHA")
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
         finally:
@@ -142,13 +156,69 @@ class Ui_Recomendaciones(object):
 
 
     def generar(self):
-        print("Hola")
+        """ Conexión al servidor de pases de datos PostgreSQL """
+        conexion = None
+        try:
+            # Lectura de los parámetros de conexion
+            params = config()
+
+            # Conexion al servidor de PostgreSQL
+            print('Conectando a la base de datos PostgreSQL...')
+            conexion = psycopg2.connect(**params)
+            # creación del cursor
+            cur = conexion.cursor()
+            # Ejecución la consulta para obtener la conexión
+            print('La version de PostgreSQL es la:')
+            cur.execute('SELECT version()')
+
+            # Se obtienen los resultados
+            db_version = cur.fetchone()
+            # Se muestra la versión por pantalla
+            print(db_version)
+            cur.execute(
+            '''SELECT customer.email, genre.name, COUNT(genre.genreid)
+                FROM creador_track
+                JOIN customer ON customer.customerid = creador_track.creadorid
+                JOIN track ON creador_track.trackid = track.trackid 
+                JOIN genre ON track.genreid = genre.genreid
+                WHERE customer.customerid IN (
+                SELECT creador_track.creadorid 
+                FROM creador_track
+                GROUP BY creador_track.creadorid
+                ORDER BY COUNT(creador_track.creadorid) DESC LIMIT 10)
+                GROUP BY customer.email, genre.genreid
+                ORDER BY customer.email DESC
+                '''
+            )
+            resul = cur.fetchall()
+            for email, genre, cuenta in resul:
+                if cuenta > 5:
+                    a = coleccion.aggregate([{ '$match': { '_id': email } },{ '$unwind': '$tracks' },{ '$match': { 'tracks.genre': genre }},{ '$project': { 'track': '$tracks.track', 'genre': '$tracks.genre' }}])
+                    for i in a:
+                        print(i['_id'],' ',i['track'],' ',i['genre'])
+            
+
+            
+
+            
+            # Recorremos los resultados y los mostramos
+            print("--------------------------------------------------")
+
+            # Cerremos el cursor
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conexion is not None:
+                conexion.close()
+                print('Conexión finalizada.')
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
+    ##RECORDAR QUITAR ID
     ui = Ui_Recomendaciones()
     ui.setupUi(Form)
     Form.show()
